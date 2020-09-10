0249f74e39dc680eb008742eb314ab2e46804d56
PierreM0 <pierreemmanuel.devin@turtlebee.site>
2020-09-10 18:41:00 +0200

    supprimer_val à corriger (ca supprime la valeur suivante)

--- listes_chainees_partiel.py
+++ listes_chainees_partiel.py
@@ -1,5 +1,5 @@
-tab = [None, None]	
-	
+tab = [None, None]
+
 def ajouter(tab, element):
     next = tab #On lui donne la valeur de la liste
     while next[1] != None: #On incrémente i jusqu'a qu'on trouve le "None" du dernier tableau
@@ -27,7 +27,7 @@ def longueur(tab):
         i += 1
         next = next[1] #A chaque fois on prend le tableau suivant
     return i+1 #On ajoute 1 car il y a une valeur en trop
-    
+
 def supprimer_ind(tab, index):
     next = tab #On lui donne la valeur de la liste
     for _ in range(index-1): #On continue jusqu'à l'index-1 donné pour avoir le tableau juste avant celui de l'index
@@ -43,3 +43,9 @@ def modifier(tab, index, value):
 def vider(tab):
     tab = [None, None] #Je remet les valeurs initiales
 
+def supprimer_val(tab,value):
+	next = tab
+	while next[0] != value:
+		next = next[1]
+	next[1] = next[1][1]
+	next[0] = next[1][0]
