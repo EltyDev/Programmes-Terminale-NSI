1fef0067abdc5a2ce84e62bd33b5e34902335f21
Venodez <30352734+Venodez@users.noreply.github.com>
2020-09-10 12:47:33 +0200

    Add files via upload

---
+++ Liste.py
@@ -0,0 +1,51 @@
+class Liste:
+
+    def __init__(self, *elements):
+        self.list = [None, None]
+        for element in elements:
+            if self.list[0] == None:
+                self.list[0] = element
+                next = self.list
+            else:
+                next[1] = [element, None]
+                next = next[1]
+    
+    def add(self, element):
+        next = self.list
+        while next[1] != None:
+            next = next[1]
+        next[1] = [element, None]
+
+    def insert(self, index, element):
+        next = self.list
+        for _ in range(index-1):
+            next = next[1]
+        next[1] = [element, next[1]]
+
+    def get(self, index):
+        next = self.list
+        for _ in range(index):
+            next = next[1]
+        print(next[0])
+
+    def __len__(self):
+        next = self.list
+        i = 0
+        if self.list[0] == None:
+            return i
+        while next[1] != None:
+            i +=  1
+            next = next[1]
+        return i+1
+    
+    def remove(self, index):
+        next = self.list
+        for _ in range(index-1):
+            next = next[1]
+        next[1] =  next[1][1]
+
+    def clear(self):
+        self.list = [None, None]
+
+    def __repr__(self):
+        return f'{self.list}'
