62225fd8b1b585d6559014ec3d5b5106d533aa04
Venodez <monsieurgamer9@gmail.com>
2020-09-22 14:36:48 +0200

    Finission de Pile

--- Python/Pile.py
+++ Python/Pile.py
@@ -1,8 +1,27 @@
 class Pile:
 
-    def __init__(self):
-    
-    def empiler():
+    def __init__(self, *elements):
+        self.valeur= []
+        for element in elements:
+            self.valeur.append(element)
 
-    def depiler():
-        
+    def entrer(self, valeur):
+        self.valeur.append(valeur)
+
+    def sortir(self):
+        valeur = self.valeur[0]
+        self.valeur.pop(0)
+        return valeur
+
+    def pile_est_vide(self):
+        if len(self) == 0:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f'{self.valeur}'
+
+    def __len__(self):
+        return len(self.valeur)
+
