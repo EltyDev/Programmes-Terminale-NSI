08443e40979d50e5cb3d300a42c9633104b27875
PierreM <pierreemmanuel.devin@turtlebee.site>
2020-09-24 14:46:20 +0200

    main

--- Python_Calc_HP/main.py
+++ Python_Calc_HP/main.py
@@ -1,2 +1,11 @@
 from Pile import *
+from math import *
 
+pile = Pile()
+
+while True:
+    prop = input(Proposition)
+    if prop == 'q':
+        break
+    if int(prop) == True:
+        pile.empiler(prop)
