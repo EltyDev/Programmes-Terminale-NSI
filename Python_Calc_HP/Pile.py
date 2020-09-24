0710fab8e9b74be4fa0f9aa2d93208b73951ca5e
PierreM <pierreemmanuel.devin@turtlebee.site>
2020-09-24 14:17:11 +0200

    Mise en place de la calc HP

---
+++ Python_Calc_HP/Pile.py
@@ -0,0 +1,33 @@
+class Pile:
+
+    def __init__(self, *elements):
+        self.valeur= []
+        for element in elements:
+            self.valeur.append(element)
+        self.valeur.reverse()
+
+    def empiler(self, valeur):
+        self.valeur.insert(0, valeur)
+
+    def depiler(self):
+        if not self.valeur:
+            return None
+        valeur = self.valeur[0]
+        self.valeur.pop(0)
+        return valeur
+
+    def pile_est_vide(self):
+        if not self.valeur:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        valeur = ""
+        for element in self.valeur:
+            valeur += str(element) + "\n" 
+        return valeur
+
+    def __len__(self):
+        return len(self.valeur)
+
