6202ab4f8ed217765729ca9d6453f8d2b1a8546c
Venodez <monsieurgamer9@gmail.com>
2020-09-24 16:41:16 +0200

    Finalisation x1

--- Python_Calc_HP/main.py
+++ Python_Calc_HP/main.py
@@ -5,30 +5,41 @@ operation, fonction, pile = ('+', '-', '*', '/', '**'), ('sin', 'cos', 'tan', 'e
 
 while True:
     prop = input('')   
-    if prop.isdigit():
+    try:
+        int(prop)
         pile.empiler(prop)
-    else:
+    except:
         if prop in operation:
             if len(pile) <= 1:
                 print(pile)
                 continue
             else:
-                resultat = eval(str(pile.valeur[1]) + prop + str(pile.valeur[0]))
-                pile.depiler()
-                pile.depiler()
-                pile.empiler(int(resultat))
-                print(resultat)
+                try:
+                    resultat = eval(str(pile.valeur[1]) + prop + str(pile.valeur[0]))
+                    pile.depiler()
+                    pile.depiler()
+                    pile.empiler(float(resultat))
+                    print(resultat)
+                except:
+                    print("Erreur: Impossible")
+                    pile.depiler()
+                    pile.depiler()
                 continue
         elif prop in fonction:
-            if len(pile) >=2:
-                print("Erreur : Apprend à compter")
+            if len(pile) < 1:
+                print("Erreur : Vous ne pouvez pas faire d'opération avec 1 ou moins de 1 élement")
                 continue
             else:
-                resultat = eval(prop + '(' + str(pile.valeur[0]) + ')')
-                pile.depiler()
-                pile.depiler()
-                pile.empiler(int(resultat))
-                print(resultat)
+                try:
+                    resultat = eval(prop + '(' + str(pile.valeur[0]) + ')')
+                    pile.depiler()
+                    pile.depiler()
+                    pile.empiler(float(resultat))
+                    print(resultat)
+                except:
+                    print("Erreur: Impossible")
+                    pile.depiler()
+                    pile.depiler()
                 continue
         elif prop == 'q':
             break
@@ -36,5 +47,3 @@ while True:
             pile = Pile()
         else:
             print("Erreur: Je suis pas programmé pour cela")
-
-
