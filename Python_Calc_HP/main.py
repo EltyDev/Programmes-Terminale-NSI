09fc119776356668ce80baf65285461624a9c341
Venodez <30352734+Venodez@users.noreply.github.com>
2020-09-29 12:42:43 +0200

    Fusion des commentaires et du code final

--- Python_Calc_HP/main.py
+++ Python_Calc_HP/main.py
@@ -6,14 +6,14 @@ operation, fonction, pile = ('+', '-', '*', '/', '**'), ('sin', 'cos', 'tan', 'e
 while True: #On brée une boucle infinie
     prop = input('$ ') #On demande chaques propositions avec un style qui fait penser au shell
     try:
-        float(prop) #On regarde si c'est une on peut la transformer en nombre décimal
+        prop = float(prop) #On regarde si c'est une on peut la transformer en nombre décimal
         pile.empiler(prop) #Et si c'est le cas, on l'empile
         print("")
         pile.voir()
     except: #Sinon
         if prop in operation: #Si la proposition est dans les la variable opérations; 
             if len(pile) <= 1:  #et si la longueur de la pile est inférieure ou égale à 1
-                print("Erreur : Vous ne pouvez pas faire d'opération avec 1 ou moins de 1 élement") #le programme ne fonctionne pas, et cela retourne un erreur
+                print("\nErreur : Vous ne pouvez pas faire d'opération avec 1 ou moins de 1 élement\n") #le programme ne fonctionne pas, et cela retourne un erreur
                 continue#on reviens au départ
             else: # si la longueur des bonne
                 try: #on essaye de voir si l'opération est faisable
@@ -21,16 +21,16 @@ while True: #On brée une boucle infinie
                     pile.depiler()#et on enlève les 2 nombres qu'on à utilisé
                     pile.depiler()
                     pile.empiler(float(resultat)) #puis on empile le résultat
-                    print(pile)
+                    print('\n' + str(resultat) + '\n')
                     continue #on reviens au départ
                 except:# si l'opération est impossible (division par zéro par exemple)
-                    print("Erreur: Impossible") #on retourne le fait que ce soit impossible
+                    print("\nErreur: Impossible\n") #on retourne le fait que ce soit impossible
                     pile.depiler()# et on permet à l'utilisateur de se corriger
                     pile.depiler()
                 continue #on reviens au départ
         elif prop in fonction: #si la proposition est dans les fonctions
             if len(pile) < 1: # et que la pile est vide 
-                print("Erreur : Vous ne pouvez pas faire d'opération si la pile est vide")
+                print("\nErreur : Vous ne pouvez pas faire d'opération avec moins de 1 élement\n"
                 continue #on reviens ou départ 
             else:# si la pile à au moins un élément
                 try:#on essaye si la fonction est possible 
@@ -38,9 +38,9 @@ while True: #On brée une boucle infinie
                     pile.depiler() #on dépile les nombres utilisés 
                     pile.depiler()
                     pile.empiler(float(resultat)) #et on empile la réponse
-                    print(pile)
+                    print('\n' + str(resultat) + '\n')
                 except:#et si l'opération est impossible (log(-n)) 
-                    print("Erreur: Impossible")#on revoies que c'est impossible
+                    print("\nErreur: Impossible\n")#on revoies que c'est impossible
                     pile.depiler()#et on laisse l'utilisateur se corriger 
                     pile.depiler()
                 continue
@@ -50,8 +50,10 @@ while True: #On brée une boucle infinie
             pile = Pile()
         elif prop == '': #si la proposition est vide, affiche la pile si elle est non vide
             if len(pile) == 0:
-                print('Vide')
+                print('\nVide\n')
             else:
-                print('\n' + str(pile))
+                print('')
+                pile.voir()
         else: #sinon on revoies une erreur
-            print("Erreur: Je suis pas programmé pour cela.")
+            print("\nErreur: Je suis pas programmé pour cela.\n")
+            pile.depiler()
